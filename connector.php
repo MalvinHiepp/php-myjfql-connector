5d5357ffec6cb0d0196ca4ef4889c30d3c57266c
Schnoggy <69483571+Schnoggy@users.noreply.github.com>
2021-05-17 17:28:07 +0200

    Uploaded Connector.php

---
+++ connector.php
@@ -0,0 +1,30 @@
+<?php
+
+    /*******************************************************************************
+    * Created by: Malvin H.                                                        *
+    * User: Schnoggy                                                               *
+    * Date: 17/05/2021                                                             *
+    * Project: Connector-JFQL                                                      *
+    * Information: MIT license                                                     *             
+    ********************************************************************************/
+
+    function query($url, $name, $password, $query){
+        $ch = curl_init();
+
+        $postRequest = array(
+            'name' => $name,
+            'password' => $password,
+            'query' => $query
+        );
+        
+        curl_setopt($ch, CURLOPT_URL, $url);
+        
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postRequest));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        
+        $output = curl_exec($ch);
+        
+        return $output;
+        curl_close($ch);
+}
+?>
